efc8248732dc0f66259f82f15edd2929096f1fdd
Garret Simpson <garretsimpson@yahoo.com>
2021-03-03 23:18:27 -0800

    Replace for loops with Array.map().

--- 2020/Day4/main.js
+++ 2020/Day4/main.js
@@ -13,7 +13,7 @@ const INPUT_FILENAME = 'input.txt';
 /**
  * Read input file.
  * @param {String} filename
- * @return {String} file contents
+ * @return {String} Returns entire contents of the file.
  */
 function readFile(filename) {
     console.log('Read input file:', INPUT_FILENAME);
@@ -35,6 +35,36 @@ function parseItems(data) {
     return data.toString().trim().split(/(?:\r?\n){2}/);
 }
 
+/**
+ * Parse record.
+ * @param {String} item
+ * @return {Object} A record object.
+ */
+function parseRecord(item) {
+    const RE = /(?<key>\w{3}):(?<value>.+?)(?: |$)/mg;
+    let a = [];
+    let o = {};
+    while ((a = RE.exec(item)) != null) {
+        if (a.groups == undefined) {
+            console.error('Unable to parse item:', item)
+            exit();
+        }
+        o[a.groups.key] = a.groups.value;
+    }
+    return o;
+}
+
+/**
+ * Verify record.
+ * @param {Object} record
+ * @return {Boolean} Returns true if record is valid. Otherwise returns false.
+ */
+function verifyRecord(record) {
+    const REQ_FIELDS = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
+    const values = REQ_FIELDS.map(key => record[key]);
+    return values.every(value => value != undefined);
+}
+
 function main() {
     console.log(TITLE);
     console.log(Date());
@@ -42,47 +72,14 @@ function main() {
 
     const input = readFile(INPUT_FILENAME);
     const items = parseItems(input);
-    const numItems = items.length;
-    console.log('Number of items:', numItems);
-    console.log('Last item:', items[numItems - 1]);
+    console.log('Total items:', items.length);
+    const records = items.map(parseRecord);
+    console.log('Total records:', records.length)
     console.log('');
 
     console.log('Part 1...');
-    // TODO: Use Array map/reduce
-
-    // Parse records
-    let records = [];
-    const RE = /(?<key>\w{3}):(?<value>.+?)(?: |$)/mg;
-    for (const item of items) {
-        let a = [];
-        let o = {};
-        while ((a = RE.exec(item)) != null) {
-            if (a.groups == undefined) {
-                console.error('Unable to parse item:', item)
-                exit();
-            }
-            o[a.groups.key] = a.groups.value;
-        }
-        records.push(o);
-    }
-    console.log('Total records:', records.length)
-
-    // Verify records
-    const REQ_FIELDS = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
-    let numValid = 0;
-    for (const record of records) {
-        let valid = true;
-        for (const field of REQ_FIELDS) {
-            if (record[field] == undefined) {
-                valid = false;
-                break;
-            }
-        }
-        if (valid) {
-            numValid++;
-        }
-    }
-    console.log('Valid Records:', numValid);
+    const numValid = records.map(verifyRecord).filter(x => x == true).length;
+    console.log('Valid records:', numValid);
 }
 
 main();
