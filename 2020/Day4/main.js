d9bdbcded1ccb485c364b209e7996a405b5927e7
Garret Simpson <garretsimpson@yahoo.com>
2021-03-04 21:21:35 -0800

    Completed Part 2

--- 2020/Day4/main.js
+++ 2020/Day4/main.js
@@ -13,7 +13,7 @@ const INPUT_FILENAME = 'input.txt';
 /**
  * Read input file.
  * @param {String} filename
- * @return {String} Returns entire contents of the file.
+ * @return {String} Return entire contents of the file.
  */
 function readFile(filename) {
     console.log('Read input file:', INPUT_FILENAME);
@@ -41,7 +41,7 @@ function parseItems(data) {
  * @return {Object} A record object.
  */
 function parseRecord(item) {
-    const RE = /(?<key>\w{3}):(?<value>.+?)(?: |$)/mg;
+    const RE = /(?<name>\w{3}):(?<value>.+?)(?: |$)/mg;
     let a = [];
     let o = {};
     while ((a = RE.exec(item)) != null) {
@@ -49,20 +49,134 @@ function parseRecord(item) {
             console.error('Unable to parse item:', item)
             exit();
         }
-        o[a.groups.key] = a.groups.value;
+        o[a.groups.name] = a.groups.value;
     }
     return o;
 }
 
+const REQ_FIELDS = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
+
 /**
- * Verify record.
+ * Verify record - Part 1.
  * @param {Object} record
- * @return {Boolean} Returns true if record is valid. Otherwise returns false.
+ * @return {Boolean} Return true if record is valid. Otherwise return false.
  */
-function verifyRecord(record) {
-    const REQ_FIELDS = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
-    const values = REQ_FIELDS.map(key => record[key]);
+function verifyRecordPart1(record) {
+    const values = REQ_FIELDS.map(name => record[name]);
     return values.every(value => value != undefined);
+}
+
+/**
+ * Verify year.
+ * @param {String} value
+ * @param {Number} min
+ * @param {Number} max
+ * @return {Boolean} Return true if value is at least min and at most max. Otherwise return false.
+ */
+function verifyYear(value, min, max) {
+    const RE = /^(?<num>\d{4})$/;
+    const a = RE.exec(value);
+    if (a == null) {
+        return false;
+    }
+    const n = parseInt(a[0], 10);
+    return (n >= min) && (n <= max);
+}
+
+/**
+ * Verify height.
+ * @param {String} value
+ * @return {Boolean} Return true if value is a valid height. Otherwise return false.
+ */
+function verifyHeight(value) {
+    const RE = /^(?<num>\d+)(?<unit>cm|in)$/;
+    const a = RE.exec(value);
+    if (a == null) {
+        return false;
+    }
+    const num = parseInt(a.groups.num, 10);
+    switch (a.groups.unit) {
+        case 'cm':
+            return (num >= 150) && (num <= 193);
+        case 'in':
+            return (num >= 59) && (num <= 76);
+    }
+}
+
+/**
+ * Verify color.
+ * @param {String} value
+ * @return {Boolean} Return true if value is a valid color. Otherwise return false.
+ */
+function verifyColor(value) {
+    const RE = /^#[0-9a-f]{6}$/;
+    return RE.test(value);
+}
+
+const VALID_EYE_COLORS = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
+/**
+ * Verify emumerated type.
+ * @param {String} value
+ * @param {Array<String>} validValues
+ * @return {Boolean} Return true if value is one of a list of valid values. Otherwise return false.
+ */
+function verifyEnum(value, validValues) {
+    const RE = new RegExp('^' + VALID_EYE_COLORS.join('|') + '$');
+    return RE.test(value);
+}
+
+/**
+ * Verify number.
+ * @param {String} value
+ * @param {Number} length
+ * @return {Boolean} Return true if value is a valid number of specified length. Otherwise return false.
+ */
+function verifyNumber(value, length) {
+    const RE = new RegExp('^\\d{' + length + '}$');
+    return RE.test(value);
+}
+
+/**
+ * Verify record - Part 2.
+ * @param {Object} record
+ * @return {Boolean} Return true if record is valid. Otherwise return false.
+ */
+function verifyRecordPart2(record) {
+    let valid = true;
+    for (const name of REQ_FIELDS) {
+        const value = record[name];
+        if (value == undefined) {
+            return false;
+        }
+        switch (name) {
+            case 'byr':
+                valid = verifyYear(value, 1920, 2002);
+                break;
+            case 'iyr':
+                valid = verifyYear(value, 2010, 2020);
+                break;
+            case 'eyr':
+                valid = verifyYear(value, 2020, 2030);
+                break;
+            case 'hgt':
+                valid = verifyHeight(value);
+                break;
+            case 'hcl':
+                valid = verifyColor(value);
+                break;
+            case 'ecl':
+                valid = verifyEnum(value, VALID_EYE_COLORS);
+                break;
+            case 'pid':
+                valid = verifyNumber(value, 9);
+                break;
+        }
+        if (!valid) {
+            console.log('Invalid field:', name, value);
+            break;
+        }
+    }
+    return valid;
 }
 
 function main() {
@@ -78,7 +192,12 @@ function main() {
     console.log('');
 
     console.log('Part 1...');
-    const numValid = records.map(verifyRecord).filter(x => x == true).length;
+    let numValid = records.map(verifyRecordPart1).filter(x => x).length;
+    console.log('Valid records:', numValid);
+    console.log('');
+
+    console.log('Part 2...');
+    numValid = records.map(verifyRecordPart2).filter(x => x).length;
     console.log('Valid records:', numValid);
 }
 
