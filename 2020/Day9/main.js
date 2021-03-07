8d21ed136a452a34378bf1d641f67d0c0bb2eaa4
Garret Simpson <garretsimpson@yahoo.com>
2021-03-06 22:13:17 -0800

    Completed Part 1

--- 2020/Day9/main.js
+++ 2020/Day9/main.js
@@ -11,6 +11,7 @@ const TITLE = 'Advent of Code 2020 - Day 9';
 const INPUT_FILENAME = 'input.txt';
 
 const EOL = /\r?\n/;
+let NUMBERS = [];
 
 /**
  * Read input file.
@@ -37,6 +38,40 @@ function parseItems(data) {
     return data.toString().trim().split(EOL);
 }
 
+/**
+ * Find the first "bad" number.
+ * @return {Number}
+ */
+function findBadNumber() {
+    const RANGE = 25;
+
+    for (let i = RANGE; i < NUMBERS.length; i++) {
+        let num = NUMBERS[i];
+        if (any2(num, i - RANGE, i) == false) {
+            return num;
+        }
+    }
+}
+
+/**
+ * Given a target number and two indexes, return true if the sum of any two numbers equals the target number.
+ * @param {Number} num
+ * @param {Number} min The lower index [inclusive].
+ * @param {Number} max The upper index [exclusive].
+ * @return {Boolean}
+ */
+function any2(num, min, max) {
+    for (let i = min; i < max; i++) {
+        for (let j = i + 1; j < max; j++) {
+            console.log(i, j, num, NUMBERS[i], NUMBERS[j]);
+            if (NUMBERS[i] + NUMBERS[j] == num) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 function main() {
     console.log(TITLE);
     console.log(Date());
@@ -45,9 +80,12 @@ function main() {
     const input = readFile(INPUT_FILENAME);
     const items = parseItems(input);
     console.log('Total items:', items.length);
+    NUMBERS = items.map(x => parseInt(x));
     console.log('');
 
     console.log('Part 1...');
+    const num1 = findBadNumber();
+    console.log("Number:", num1)
     console.log('');
 }
 
