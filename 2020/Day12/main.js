f7a2cb133906b8de605dfed6b47fb06dc3c0f36d
Garret Simpson <garretsimpson@yahoo.com>
2021-03-07 21:50:23 -0800

    Initial version

---
+++ 2020/Day12/main.js
@@ -0,0 +1,54 @@
+/**
+ * Advent of Code 2020
+ * 
+ * @author: Garret Simpson
+ */
+
+const { readFileSync } = require('fs');
+const { exit } = require('process');
+
+const TITLE = 'Advent of Code 2020 - Day 12';
+const INPUT_FILENAME = 'input.txt';
+
+const EOL = /\r?\n/;
+
+/**
+ * Read input file.
+ * @param {String} filename
+ * @return {String} Return entire contents of the file.
+ */
+function readFile(filename) {
+    console.log('Read input file:', INPUT_FILENAME);
+    try {
+        return readFileSync(INPUT_FILENAME, 'utf8')
+    } catch (err) {
+        console.error(err)
+        exit();
+    }
+}
+
+/**
+ * Parse items.
+ * @param {String} data
+ * @return {Array<Array<String>>} An array of items.
+ */
+function parseItems(data) {
+    console.log('Parsing...');
+    return data.toString().trim().split(EOL);
+}
+
+function main() {
+    console.log(TITLE);
+    console.log(Date());
+    console.log('');
+
+    const input = readFile(INPUT_FILENAME);
+    const items = parseItems(input);
+    console.log('Total items:', items.length);
+    console.log('');
+
+    console.log('Part 1...');
+    console.log('');
+}
+
+main();
