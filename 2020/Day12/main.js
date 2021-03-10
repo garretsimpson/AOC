ea925bbf1232dd633d2c97605fc9f6cdec54fe3d
Garret Simpson <garretsimpson@yahoo.com>
2021-03-10 13:33:59 -0800

    Completed Part 1

--- 2020/Day12/main.js
+++ 2020/Day12/main.js
@@ -4,13 +4,12 @@
  * @author: Garret Simpson
  */
 
-const { readFileSync } = require('fs');
-const { exit } = require('process');
-
 const TITLE = 'Advent of Code 2020 - Day 12';
 const INPUT_FILENAME = 'input.txt';
 
 const EOL = /\r?\n/;
+
+const SHIP = { x: 0, y: 0, dir: 0 };
 
 /**
  * Read input file.
@@ -18,13 +17,9 @@ const EOL = /\r?\n/;
  * @return {String} Return entire contents of the file.
  */
 function readFile(filename) {
+    const { readFileSync } = require('fs');
     console.log('Read input file:', INPUT_FILENAME);
-    try {
-        return readFileSync(INPUT_FILENAME, 'utf8')
-    } catch (err) {
-        console.error(err)
-        exit();
-    }
+    return readFileSync(INPUT_FILENAME, 'utf8')
 }
 
 /**
@@ -37,17 +32,77 @@ function parseItems(data) {
     return data.toString().trim().split(EOL);
 }
 
+/**
+ * Run rules.
+ * @param {Array<String>} rules
+ */
+function runRules(rules) {
+    RE = /(?<op>\w)(?<num>\d+)/;
+    for (rule of rules) {
+        console.log('Rule:', rule);
+        a = RE.exec(rule);
+        if (a == null || a.groups == undefined) {
+            log.error('Unable to parse rule:', rule);
+            return;
+        }
+        op = a.groups.op;
+        if (op == 'F') {
+            switch (SHIP.dir) {
+                case 0:
+                    op = 'E';
+                    break;
+                case 90:
+                    op = 'N';
+                    break;
+                case 180:
+                    op = 'W';
+                    break;
+                case 270:
+                    op = 'S';
+                    break;
+            }
+        }
+        num = parseInt(a.groups.num);
+        switch (op) {
+            case 'N':
+                SHIP.y -= num;
+                break;
+            case 'S':
+                SHIP.y += num;
+                break;
+            case 'E':
+                SHIP.x += num;
+                break;
+            case 'W':
+                SHIP.x -= num;
+                break;
+            case 'L':
+                SHIP.dir += num;
+                SHIP.dir %= 360;
+                break;
+            case 'R':
+                SHIP.dir -= num;
+                if (SHIP.dir < 0)
+                    SHIP.dir += 360;
+                break;
+        }
+    }
+}
+
 function main() {
     console.log(TITLE);
     console.log(Date());
     console.log('');
 
     const input = readFile(INPUT_FILENAME);
-    const items = parseItems(input);
-    console.log('Total items:', items.length);
+    const rules = parseItems(input);
+    console.log('Total rules:', rules.length);
     console.log('');
 
     console.log('Part 1...');
+    runRules(rules);
+    d1 = Math.abs(SHIP.x) + Math.abs(SHIP.y);
+    console.log('Distance:', d1);
     console.log('');
 }
 
