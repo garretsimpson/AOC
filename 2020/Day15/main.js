fd0fe421ca4c367c0f24dc8f85391259b535eec9
Garret Simpson <garretsimpson@yahoo.com>
2021-03-12 12:29:11 -0800

    Completed Part 1

---
+++ 2020/Day15/main.js
@@ -0,0 +1,32 @@
+/**
+ * Advent of Code 2020
+ * 
+ * @author: Garret Simpson
+ */
+
+const TITLE = 'Advent of Code 2020 - Day 15';
+
+function main() {
+    console.log(TITLE);
+    console.log(Date());
+    console.log('');
+
+    const input = '0,1,4,13,15,12,16';
+    console.log('Input:', input);
+    const items = input.split(/,/);
+    const values = items.map(item => parseInt(item));
+    console.log('');
+
+    console.log('Part 1...');
+    log = [];
+    values.forEach(val => log.unshift(val));
+    while (log.length < 2020) {
+        let pos = log.indexOf(log[0], 1);  // previously seen position
+        let val = (pos == -1) ? 0 : pos;
+        log.unshift(val);
+    }
+    console.log('Result:', log[0]);
+    console.log('');
+}
+
+main();
