f8a3c91fb415ec9e84e9d3a96f1e5a9a71c87021
Garret Simpson <garretsimpson@yahoo.com>
2021-03-13 00:31:18 -0800

    Working on Part 2

--- 2020/Day15/main.js
+++ 2020/Day15/main.js
@@ -18,7 +18,7 @@ function main() {
     console.log('');
 
     console.log('Part 1...');
-    log = [];
+    let log = [];
     values.forEach(val => log.unshift(val));
     while (log.length < 2020) {
         let pos = log.indexOf(log[0], 1);  // previously seen position
@@ -29,12 +29,24 @@ function main() {
     console.log('');
 
     console.log('Part 2...');
-    while (log.length < 30000000) {
+    log = new Map();
+    values.forEach(val => log.set(val, null));
+    let count = values.length;
+    let last = values[values.length - 1];
+//    while (count < 30000000) {
+    while (count < 2020) {
         if (log.length % 100000 == 0)
             console.log(log.length);
-        let pos = log.indexOf(log[0], 1);  // previously seen position
-        let val = (pos == -1) ? 0 : pos;
-        log.unshift(val);
+        let pos = log.get(last);  // previously seen position
+        let val = 0;
+        if (pos == null) {
+            log.set(0, null);
+        }
+        else {
+            last = val = count - pos;
+            log.set(last, count);
+            log.set(val, val);
+        }
     }
     console.log('Result:', log[0]);
     console.log('');
